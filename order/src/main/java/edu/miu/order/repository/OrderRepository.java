3645c8df383fbc62c564411d87eee7d052a898cb
yemane <yemane.hadis@gmail.com>
2022-11-21 17:19:09 -0600

    Typo

--- order/src/main/java/edu/miu/order/repository/OrderRepository.java
+++ order/src/main/java/edu/miu/order/repository/OrderRepository.java
@@ -1,2 +1,11 @@
-package edu.miu.order.repository;public class OrderRepository {
+package edu.miu.order.repository;
+
+import edu.miu.order.model.entity.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order,String> {
+
+
 }
