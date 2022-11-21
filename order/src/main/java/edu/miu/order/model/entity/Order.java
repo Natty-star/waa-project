9c5792dc3e42a4cee7e8a576bc610aab5e4014a2
yemane <yemane.hadis@gmail.com>
2022-11-21 17:18:15 -0600

    Lombok Data Annotation Added

--- order/src/main/java/edu/miu/order/model/entity/Order.java
+++ order/src/main/java/edu/miu/order/model/entity/Order.java
@@ -1,12 +1,13 @@
 package edu.miu.order.model.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @Document()
-
+@Data
 public class Order {
     @Id
     String id;
