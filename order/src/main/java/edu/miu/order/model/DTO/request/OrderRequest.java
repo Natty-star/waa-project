315fe9e6e24f8a31ab6c327da68350a95a1ad3ce
yemane <yemane.hadis@gmail.com>
2022-11-21 17:19:29 -0600

    DTO Added

--- order/src/main/java/edu/miu/order/model/DTO/request/OrderRequest.java
+++ order/src/main/java/edu/miu/order/model/DTO/request/OrderRequest.java
@@ -1,2 +1,16 @@
-package edu.miu.order.model.DTO.request;public class OrderRequest {
+package edu.miu.order.model.DTO.request;
+
+import java.time.LocalDate;
+
+public class OrderRequest {
+
+    String userId;
+
+    String propertyId;
+
+    double price;
+
+    LocalDate startDate;
+
+    LocalDate endDate;
 }
--- order/src/main/java/edu/miu/order/model/DTO/response/OrderResponse.java
+++ order/src/main/java/edu/miu/order/model/DTO/response/OrderResponse.java
@@ -1,2 +1,16 @@
-package edu.miu.order.model.DTO.response;public class OrderResponse {
+package edu.miu.order.model.DTO.response;
+
+import java.time.LocalDate;
+
+public class OrderResponse {
+
+    String userId;
+
+    String propertyId;
+
+    double price;
+
+    LocalDate startDate;
+
+    LocalDate endDate;
 }
