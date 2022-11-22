fcacc5f4f9a7e3486d3f2627673c6053221a532a
Natty Teshome <nattyteshome30@gmail.com>
2022-11-22 16:31:34 -0600

    add security

---
+++ authentication/src/main/java/edu/miu/authentication/exception/JwtTokenMalformedException.java
@@ -0,0 +1,12 @@
+package edu.miu.authentication.exception;
+
+import javax.naming.AuthenticationException;
+
+public class JwtTokenMalformedException extends AuthenticationException {
+    private static final long serialVersionUID = 1L;
+
+    public JwtTokenMalformedException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ authentication/src/main/java/edu/miu/authentication/exception/JwtTokenMissingException.java
@@ -0,0 +1,12 @@
+package edu.miu.authentication.exception;
+
+import javax.naming.AuthenticationException;
+
+public class JwtTokenMissingException extends AuthenticationException {
+    private static final long serialVersionUID = 1L;
+
+    public JwtTokenMissingException(String msg) {
+        super(msg);
+    }
+
+}
