213448e255385e0525c4319eb7c15e3d7a832ac7
Merhatsidk <merhatsidk29@gmail.com>
2022-11-22 22:37:24 -0600

    kafka config

--- property/src/main/java/edu/miu/property/PropertyApplication.java
+++ property/src/main/java/edu/miu/property/PropertyApplication.java
@@ -1,10 +1,13 @@
 package edu.miu.property;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class PropertyApplication {
@@ -22,5 +25,13 @@ public class PropertyApplication {
 //	public MongoTemplate mongoTemplate(){
 //		return new MongoTemplate();
 //	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("topic1")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
 
 }
---
+++ property/src/main/java/edu/miu/property/kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package edu.miu.property.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        log.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("propertyTopic", message);
+    }
+}
